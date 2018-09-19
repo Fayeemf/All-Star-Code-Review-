7cc886b02df8cc5b3180e11980cd05a085e44480
KingLocked <fayeemf@gmail.com>
2018-09-18 23:55:57 -0400

    Day[1]  Array Complete

--- array.js
+++ array.js
@@ -1,10 +1,20 @@
 //messing with arrays.
 let grades = ["93","100","65","84","80"];
 //inorder to access arrays, you use brackets.
-console.log(grades[0]);
-console.log(grades[1]);
-console.log(grades[2]);
-console.log(grades[3]);
-console.log(grades[4]);
+// console.log(grades[0]);
+// console.log(grades[1]);
+// console.log(grades[2]);
+// console.log(grades[3]);
+// console.log(grades[4]);
 
 //Arrays are read starting from 0 then so on and so forth
+
+const coins = [1, 5, 10, 25, 50];
+// console.log(coins[0]);
+// console.log(coins[1]);
+// console.log(coins[2]);
+// console.log(coins[3]);
+// console.log(coins[4]);
+
+    // for(let i = 0; i < coins.length; i++);
+    // console.log [i]
