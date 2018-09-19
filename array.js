89035ccd256be03d298c4fadd7b3828648f55905
KingLocked <fayeemf@gmail.com>
2018-09-18 23:42:29 -0400

    Practice Arrays Day [1]

---
+++ array.js
@@ -0,0 +1,10 @@
+//messing with arrays.
+let grades = ["93","100","65","84","80"];
+//inorder to access arrays, you use brackets.
+console.log(grades[0]);
+console.log(grades[1]);
+console.log(grades[2]);
+console.log(grades[3]);
+console.log(grades[4]);
+
+//Arrays are read starting from 0 then so on and so forth
